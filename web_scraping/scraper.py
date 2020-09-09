50865423508f55d82c995baf8e325e8a721474f7
azez-alhoot <azezalhoot82@yahoo.com>
2020-09-10 01:28:05 +0300

    fix errors

--- web_scraping/scraper.py
+++ web_scraping/scraper.py
@@ -36,6 +36,7 @@ def get_citations_needed_report(url):
     all_citations = result.find_all('p')
 
     para_lst = []
+    before_lst = []
     for i in all_citations:
         try:
             axz = i.find_all('span',string = lambda text: 'citation needed' in text.lower())
@@ -45,15 +46,23 @@ def get_citations_needed_report(url):
                     pos = i.text.index('citation')
                     qut = i.text[:pos-1]
                     stop = qut.split('. ')
-                    para_lst.append(stop[-1])
+                    before_lst.append(stop[-1])
         except AttributeError as e:
             continue
 
+    output = ''
+    for i in range(len(para_lst)):
+        output += f'Citation {i+1}\n'
+        output += '*****************\n'
+        output+= para_lst[i]+'\n'
+        output+= 'The line before \n'
+        output += '*****************\n'
+        output+= before_lst[i]+'\n'
     
-    # for i in para_lst:
-    #     print(i)
+    # with open('citation.txt','w') as f:
+    #     f.write(output)
 
-    return para_lst
+    return output
 
 
-get_citations_needed_report('https://en.wikipedia.org/wiki/Petra')
+print(get_citations_needed_report('https://en.wikipedia.org/wiki/Petra'))
